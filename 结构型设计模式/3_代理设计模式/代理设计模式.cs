7cd9b0b117b0e705b4c30a178c0ccb755ed521bd
Ikari <413826180@qq.com>
2023-05-15 17:00:30 +0800

    代理设计模式

---
+++ 结构型设计模式/3_代理设计模式/代理设计模式.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyDesignPattern.结构型设计模式._3_代理设计模式
+{
+    public class 代理设计模式
+    {
+        public void Main()
+        {
+            ClassFlower classFlower = new ClassFlower();
+            classFlower.Name = "海绵宝宝";
+
+            //创建代理对象
+            ISubject subject = new Proxy(new RealSubject(classFlower));
+
+            subject.GiveSmoking();
+            subject.GiveBeer();
+            subject.GiveJK();
+        }
+
+    }
+
+    public class Proxy : ISubject
+    {
+        private ISubject realSubject;
+
+        public Proxy(ISubject realSubject)
+        {
+            this.realSubject = realSubject;
+        }
+
+        public void GiveBeer()
+        {
+            realSubject.GiveBeer();
+        }
+
+        public void GiveJK()
+        {
+            realSubject.GiveJK();
+        }
+
+        public void GiveSmoking()
+        {
+            realSubject.GiveSmoking();
+        }
+    }
+
+    public interface ISubject
+    {
+        void GiveSmoking();
+        void GiveBeer();
+        void GiveJK();
+    }
+
+    public class ClassFlower
+    {
+        public string Name { get; set; }
+    }
+
+    public class RealSubject : ISubject
+    {
+        private ClassFlower _classFlower;
+
+        public RealSubject(ClassFlower classFlower)
+        {
+            _classFlower = classFlower;
+        }
+
+        public void GiveBeer()
+        {
+            Console.WriteLine(this._classFlower.Name + "同学请你喝酒");
+        }
+
+        public void GiveJK()
+        {
+            Console.WriteLine(this._classFlower.Name + "同学请你JK");
+        }
+
+        public void GiveSmoking()
+        {
+            Console.WriteLine(this._classFlower.Name + "同学请你抽烟");
+        }
+    }
+
+
+
+}
---
+++ 结构型设计模式/3_代理设计模式/代理设计模式应用.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyDesignPattern.结构型设计模式._3_代理设计模式
+{
+    public class 代理设计模式应用
+    {
+
+    }
+
+    public class ProxyOrder : IOrder
+    {
+        private RealOrder realOrder;
+
+        public ProxyOrder(RealOrder realOrder)
+        {
+            this.realOrder = realOrder;
+        }
+
+        public int GetOrderNum()
+        {
+            return realOrder.GetOrderNum();
+        }
+
+        public string GetOrderUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetProductName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetOrderNum(int count, string user)
+        {
+            //权限判断
+            if(user !=null && user.Equals(this.realOrder.User))
+            {
+                this.realOrder.SetOrderNum(count, user);
+            }
+            else
+            {
+                Console.WriteLine("您无权修改此数据");
+            }
+        }
+
+        public void SetOrderUser(string orderUserName, string user)
+        {
+            //权限判断
+            if (user != null && user.Equals(this.realOrder.User))
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("您无权修改此数据");
+            }
+        }
+
+        public void SetProductName(string productName, string user)
+        {
+            //权限判断
+            if (user != null && user.Equals(this.realOrder.User))
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("您无权修改此数据");
+            }
+        }
+    }
+
+    public interface IOrder
+    {
+        string GetProductName();
+        void SetProductName(string productName, string user);
+
+        int GetOrderNum();
+        void SetOrderNum(int count ,string user);
+
+        string GetOrderUser();
+        void SetOrderUser(string orderUserName, string user);
+
+    }
+
+    public class RealOrder : IOrder
+    {
+        public string ProductName
+        {
+            get;
+            set; }
+        public string User { get; set; }
+        public int OrderNum { get; set; }
+        public string OrderUser { get; set; }
+
+        public RealOrder(string productName, string user, int orderNum, string orderUser)
+        {
+            ProductName = productName;
+            User = user;
+            OrderNum = orderNum;
+            OrderUser = orderUser;
+        }
+
+        public int GetOrderNum()
+        {
+            return this.OrderNum;
+        }
+
+        public string GetOrderUser()
+        {
+            return this.User;
+        }
+
+        public string GetProductName()
+        {
+            return this.ProductName;
+        }
+
+        public void SetOrderNum(int count, string user)
+        {
+            this.User = user;
+            this.OrderNum = count;
+        }
+
+        public void SetOrderUser(string orderUserName, string user)
+        {
+            this.User = user;
+            this.OrderUser = orderUserName;
+        }
+
+        public void SetProductName(string productName, string user)
+        {
+            this.User = user;
+            this.ProductName = productName;
+        }
+    }
+
+
+
+
+
+
+
+
+}
