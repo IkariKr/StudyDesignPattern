7d7089526d35d728c4b4d0c90a07d3f57cdb4b7b
Ikari <413826180@qq.com>
2023-05-15 17:00:12 +0800

    适配器模式

--- 结构型设计模式/1_适配器设计模式/适配器设计模式.cs
+++ 结构型设计模式/1_适配器设计模式/适配器设计模式.cs
@@ -11,8 +11,35 @@ namespace StudyDesignPattern.结构型设计模式._1_适配器设计模式
     /// </summary>
     public class 适配器设计模式
     {
+        public void Main()
+        {
+            IPhoneCharge phoneCharge = new PhoneChargeAdapter();
+            phoneCharge.PhoneCharge();
+        }
+
+        
     }
 
+    public class AndroidChargeAdaptee
+    {
+        public void AndroidCharge()
+        {
+            Console.WriteLine("安卓充电线充电");
+        }
+    }
 
+    public interface IPhoneCharge
+    {
+        void PhoneCharge();
+    }
 
+    public class PhoneChargeAdapter : IPhoneCharge
+    {
+        //在Adapter中，封装了一个Adaptee对象，这个对象才是实现功能的对象
+        private AndroidChargeAdaptee androidChargeAdaptee = new AndroidChargeAdaptee();
+        public void PhoneCharge()
+        {
+            androidChargeAdaptee.AndroidCharge();
+        }
+    }
 }
